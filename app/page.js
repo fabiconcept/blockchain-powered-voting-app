6bc7224dd11a202f4feff612b4490ce27f7240d6
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2023-06-30 11:49:18 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,54 +1,8 @@
-"use client"
-import connectWeb3 from "@/utils/Functions/connectWeb3";
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, sepolia } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-
-const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, sepolia],
-  [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API }),
-    publicProvider()
-  ]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  projectId: 'votingDApp',
-  chains
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient
-})
-
 export default async function Home() {
-  const output = connectWeb3()
-  const asyncOutPut = await output;
-
-  return (
+    return (
     <main className="">
 
-      <WagmiConfig config={wagmiConfig}>
-        <RainbowKitProvider chains={chains}>
-          <ConnectButton
-            showBalance={{
-              smallScreen: false,
-              largeScreen: true,
-            }}
-          />
-          {asyncOutPut}
-        </RainbowKitProvider>
-      </WagmiConfig>
+      
     </main>
   )
 }
